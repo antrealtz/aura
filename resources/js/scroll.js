85e9a725c10c2f1dde29d6ba4d6a5025fa0e0b71
Anton <17255888+antrealtz@users.noreply.github.com>
2024-07-12 00:44:46 -0400

    added page scrolling, home and footer anchor

--- resources/js/scroll.js
+++ resources/js/scroll.js
@@ -1,3 +1,16 @@
-function scrollTo(hashName) {
-    location.hash = "#" + hashName;
-}
+const mainContainer = document.getElementById('mainContainer');
+let currentSection = 0;
+
+window.addEventListener('wheel', (event) => {
+    event.preventDefault(); // Prevent default scrolling behavior
+    
+    if (event.deltaY > 0) {
+        // Scrolling down
+        currentSection = Math.min(currentSection + 1, mainContainer.children.length - 1);
+    } else {
+        // Scrolling up
+        currentSection = Math.max(currentSection - 1, 0);
+    }
+    
+    mainContainer.children[currentSection].scrollIntoView({ behavior: 'smooth' });
+}, { passive: false }); // Ensure passive is set to false for preventDefault to work
