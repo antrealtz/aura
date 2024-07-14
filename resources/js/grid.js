475da9c75ddd379f05d4eb7cab43de55dee3154c
Anton <17255888+antrealtz@users.noreply.github.com>
2024-07-14 14:19:52 -0400

    minor fixes

--- resources/js/grid.js
+++ resources/js/grid.js
@@ -1,11 +1,8 @@
-document.querySelectorAll('#navigation .grid').forEach(item => {
-    item.addEventListener('click', function(e) {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
 
-        window.scrollTo({
-            top: targetElement.offsetTop,
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
